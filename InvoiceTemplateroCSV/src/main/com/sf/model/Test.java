9156358d213ae3b57287cb3cb27e8be1e98abd88
Naman.Sharda <Naman.Sharda@IB-SEZ-LAP-569.infobeans.in>
2020-08-05 17:44:38 +0530

    Project Setup

---
+++ InvoiceTemplateroCSV/src/main/com/sf/model/Test.java
@@ -0,0 +1,14 @@
+package com.sf.model;
+
+public class Test {
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	        System.out.println( "Hello World!" );
+	        System.out.println( "Hello World!" );
+	        System.out.println( "Hello World!" );
+	        System.out.println( "Hello World!" );
+	        System.out.println( "Hello World!" );
+	    }
+}
