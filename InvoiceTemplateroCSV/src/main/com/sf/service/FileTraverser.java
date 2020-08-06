db0803fe617cf1d9576340ad3dd0ca1e59f3f62c
Pratik <pratik.gupta@infobeans.com>
2020-08-06 11:54:25 +0530

    Adding files for traverse and parse

---
+++ InvoiceTemplateroCSV/src/main/com/sf/service/FileTraverser.java
@@ -0,0 +1,22 @@
+package com.sf.service;
+
+import java.io.File;
+
+//Traverse Each file in a folder
+public class FileTraverser {
+	static void traverseFiles(File[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i].getName());
+			//call the template parser method for parsing logic
+		}
+	}
+
+	public static void main(String[] args) {
+		String path = "E:\\SalseForce\\Community Portal\\Invoice Template";
+		File maindir = new File(path);
+		if (maindir.exists() && maindir.isDirectory()) {
+			File arr[] = maindir.listFiles();
+			traverseFiles(arr);
+		}
+	}
+}
---
+++ InvoiceTemplateroCSV/src/main/com/sf/service/TemplateParser.java
@@ -0,0 +1,9 @@
+//Recursive Java program to print all files 
+//in a folder(and sub-folders) 
+
+package com.sf.service;
+
+public class TemplateParser 
+{ 
+
+} 
