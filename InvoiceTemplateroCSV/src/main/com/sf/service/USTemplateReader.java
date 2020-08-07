4b6676524d0bc46cd9fd4c2a1327b11e2d47afec
Pratik <pratik.gupta@infobeans.com>
2020-08-07 16:30:40 +0530

    Changes for property file reading and US template reading

--- InvoiceTemplateroCSV/src/main/com/sf/service/USTemplateReader.java
+++ InvoiceTemplateroCSV/src/main/com/sf/service/USTemplateReader.java
@@ -24,39 +24,27 @@ public class USTemplateReader implements ExcelTemplateReader {
 	public void parseExcel(File excel, List<InvoiceCSV> invoiceList) throws IOException {
 		
 		InvoiceCSV invoice = new InvoiceCSV();
-		
-		// TODO Auto-generated method stub
-
-		// File myFile = new File("E:\\SalseForce\\Community Portal\\Invoice Template");
-
 		FileInputStream fis = new FileInputStream(excel);
-
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
 		// creating a Sheet object to retrieve the object
 		XSSFSheet sheet = wb.getSheetAt(0);
 
-		CellReference cellReference = new CellReference("M38");
+		CellReference accountNameReferance = new CellReference("C14");
+		String accountName = SfUtils.getCellValueasString(sheet, accountNameReferance);
 
-		Row row = sheet.getRow(cellReference.getRow());
-		Cell cell = row.getCell(cellReference.getCol());
-
-		Double V1 = SfUtils.getCellValueasNumber(sheet, cellReference);
-
-		System.out.println("*************************");
-
-		System.out.println(V1);
-
-		System.out.println("*************************");
-		
+		CellReference amountReferance = new CellReference("K40");
+		Double amount = SfUtils.getCellValueasNumber(sheet, amountReferance);
 
 		invoice.setCurrency(Constants.Currency_USD);
 		invoice.setBillingType(Constants.BillingType_Monthly);
 		invoice.setInvoiceStatus(Constants.InvoiceStatus_Draft);
-		//Account Name
-		//Amount
 		
-		invoiceList.add(invoice);
+		String accountId = SfUtils.getAccountId(accountName);
+		invoice.setAccount_Name(accountId);
+		invoice.setAmount(amount);
 
+		invoiceList.add(invoice);
+		System.out.println(invoiceList);
 	}
 
 }
